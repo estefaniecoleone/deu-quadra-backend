755741dc687ac1e419bb6aab6a6f1b51263143e7
Estefanie Coleone <84871976+estefaniecoleone@users.noreply.github.com>
2024-03-14 23:27:09 -0300

    commit inicial

---
+++ deuquadra/src/main/java/ifsp/edu/br/deuquadra/DeuquadraApplication.java
@@ -0,0 +1,13 @@
+package ifsp.edu.br.deuquadra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeuquadraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeuquadraApplication.class, args);
+	}
+
+}
---
+++ deuquadra/src/test/java/ifsp/edu/br/deuquadra/DeuquadraApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsp.edu.br.deuquadra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeuquadraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
